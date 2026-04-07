496d052deee7a86d9cf6dfa6a2e1d24ca9792fbd
Aishwaryakumari155 <aishwaryakumari155@gmail.com>
2026-04-07 09:08:07 +0530

    Add files via upload

---
+++ python practice class.py
@@ -0,0 +1,10 @@
+
+name=input("Enter name:")
+yob=int(input("Enter year of birth:"))
+print(name)
+print(year)
+if year<=1966
+print("You are a senior citizen")
+else("You are not a Senior citizen")
+        
+]
