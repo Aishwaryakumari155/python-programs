67f753a1b74c091c880a79ccf8e6d68383822df4
Aishwaryakumari155 <aishwaryakumari155@gmail.com>
2026-05-27 12:23:52 +0530

    Delete prog13.py

--- prog13.py
+++
@@ -1,9 +0,0 @@
-lines = [
-    "This is the first line.",
-    "And here's the second line.",
-    "The third line concludes the example."
-]
-
-with open("another_file.txt", "w") as file:
-    for line in lines:
-        file.write(line + "\n")  
